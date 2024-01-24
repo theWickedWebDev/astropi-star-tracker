b8bd43e047224b8c8b320022b632093d1f86b313
Stephen Young <stephen@thewickedweb.dev>
2024-01-24 17:12:24 -0500

    Move camera out, update for RPi5 (#27)
    * Move camera out, update for RPi5
    
    * Updated motor gpio to use gpiod for compatibility with RPi5
    * Removed everything unrelated to telescope control
    
    * Stability tweaks
    
    - Reduce max speed, increase max accel/decel
    - Set run.sh to use chrt with a high priority
    
    * Update poetry.lock
    
    ---------
    
    Co-authored-by: Stephen Young <stephen@theyoungs.cloud>
    Co-authored-by: Christopher Chambers <chris@peanutcode.com>

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
-poetry shell
-python3 ./src/main.py
+
+# poetry shell
+# poetry run -- python -m src.main
+
+# poetry shell
+poetry_path="$(which poetry)"
+sudo -E chrt -f 90 sudo -E -u pi "$poetry_path" run -- python -m src.main
+
+# poetry shell
+# find . -name '*.py' | entr -cr python -m src.main
