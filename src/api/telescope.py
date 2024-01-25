f90f3cb60be0b0c4abf2d4956329608ea90ce04b
Chris Chambers <chris-chambers@users.noreply.github.com>
2024-01-25 12:42:02 -0500

    Add status reporting for telescope control activities (#28)

--- src/api/telescope.py
+++ src/api/telescope.py
@@ -2,8 +2,10 @@ from quart import current_app, request
 
 from astropy.coordinates import ICRS, SkyCoord
 from astropy.time import Time
+import trio
 
 from .. import telescope_control as tc
+from ..activity import ActivityStatus
 from ._blueprint import api
 from .response import returnResponse
 
@@ -19,14 +21,29 @@ def get_telescope() -> tc.TelescopeControl:
     return telescope
 
 
+async def _final_status(
+    chan: trio.MemoryReceiveChannel[ActivityStatus],
+) -> ActivityStatus | None:
+    status = None
+    async for status in chan:
+        pass
+    return status
+
+
 @api.route("/calibrate/", methods=["POST"])
 async def calibrate():
     try:
         _ra = request.args.get("ra")
         _dec = request.args.get("dec")
-        get_telescope().calibrate(
-            tc.FixedTarget(SkyCoord(ra=_ra, dec=_dec, frame=ICRS))
+
+        status = await _final_status(
+            get_telescope().calibrate(
+                tc.FixedTarget(SkyCoord(ra=_ra, dec=_dec, frame=ICRS))
+            )
         )
+
+        # ActivityStatus.COMPLETE = good, ActivityStatus.ABORTED = maybe bad
+        print(status)
 
         return await returnResponse({"calibrated": True, "ra": _ra, "dec": _dec}, 200)
 
@@ -38,7 +55,12 @@ async def calibrate():
 async def calibrate_by_name():
     try:
         _name = request.args.get("name")
-        get_telescope().calibrate(tc.FixedTarget(SkyCoord.from_name(_name)))
+        status = await _final_status(
+            get_telescope().calibrate(tc.FixedTarget(SkyCoord.from_name(_name)))
+        )
+
+        # ActivityStatus.COMPLETE = good, ActivityStatus.ABORTED = maybe bad
+        print(status)
 
         return await returnResponse({"calibrated": True, "name": _name}, 200)
     except:
@@ -53,10 +75,19 @@ async def calibrate_bump():
         sync = _bool_type(True)(request.args.get("sync", "true"))
 
         telescope = get_telescope()
-        telescope.calibrate_rel_steps(bearing, dec)
+        chan = telescope.calibrate_rel_steps(bearing, dec)
         target = telescope.target
         if sync and target is not None:
+            # NOTE: telescope.track also returns a channel of statuses, but it
+            # is more difficult to use it, since the track operation could last
+            # forever.
             telescope.track(target)
+
+        # NOTE: It's important to wait for the final status _after_ issuing the
+        # track command, so we don't cause an unwanted delay.
+        status = await _final_status(chan)
+        # ActivityStatus.COMPLETE = good, ActivityStatus.ABORTED = maybe bad
+        print(status)
 
         return await returnResponse(
             {"calibrated": True, "bearing": bearing, "dec": dec}, 200
@@ -70,7 +101,12 @@ async def calibrate_solar_system_object():
     try:
         _name = request.args.get("name")
 
-        get_telescope().calibrate(tc.SolarSystemTarget(_name))
+
+        status = await _final_status(
+            get_telescope().calibrate(tc.SolarSystemTarget(_name))
+        )
+        # ActivityStatus.COMPLETE = good, ActivityStatus.ABORTED = maybe bad
+        print(status)
 
         return await returnResponse(
             {
@@ -95,6 +131,12 @@ async def goto():
         _ra = request.args.get("ra")
         _dec = request.args.get("dec")
 
+        # REVIEW: Here, I'm guessing we'll need something fancier than what
+        # currently exists.  This `track` consists of a slew followed by
+        # tracking. It doesn't make sense to notify about the tracking part,
+        # since it lasts indefinitely, but callers might want to know when the
+        # slew part has finished, and currently there is no way to get that
+        # information.
         get_telescope().track(tc.FixedTarget(SkyCoord(ra=_ra, dec=_dec, frame=ICRS)))
 
         return await returnResponse({"goto": True, "ra": _ra, "dec": _dec}, 200)
@@ -107,6 +149,8 @@ async def goto():
 async def goto_by_name():
     try:
         _name = request.args.get("name")
+
+        # REVIEW: See comment on `goto`.
         get_telescope().track(tc.FixedTarget(SkyCoord.from_name(_name)))
 
         return await returnResponse({"goto": True, "name": _name}, 200)
@@ -119,6 +163,7 @@ async def goto_by_mpc_query():
     try:
         name = request.args.get("name")
         assert name is not None
+        # REVIEW: See comment on `goto`.
         get_telescope().track(tc.MPCQueryTarget(name))
 
         return await returnResponse({"goto": True, "name": name}, 200)
@@ -130,6 +175,7 @@ async def goto_by_mpc_query():
 async def goto_solar_system_object():
     try:
         _name = request.args.get("name")
+        # REVIEW: See comment on `goto`.
         get_telescope().track(tc.SolarSystemTarget(_name))
         return await returnResponse(
             {
